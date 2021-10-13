db9eb9ec308d31c4e186cd9affee4a6b00ae3778
Brett Wilcox <brett@scorpion.io>
2021-10-13 14:20:19 -0500

    docker updates

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Demo App</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation.</p>
+
+<h1>App Management</h1>
+<ul>
+    <li><a href="http://localhost:3001" target="_blank">Grafana</a></li>
+    <li><a href="http://localhost:8200" target="_blank">Vault</a></li>
+</ul>
