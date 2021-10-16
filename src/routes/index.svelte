db68cadc7ab3367c55a9276bb97ba965bc2686c1
Brett Wilcox <brett@scorpion.io>
2021-10-16 11:41:54 -0500

    Add Jaeger + OTEL

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,11 @@
-<h1>Demo App</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation.</p>
+<h1>Scorpion Pipeline</h1>
+<p>Data Pipeline for the scorpion project.</p>
 
 <h1>App Management</h1>
 <ul>
 	<li><a href="http://localhost:3001" target="_blank">Grafana</a></li>
 	<li><a href="http://localhost:9090" target="_blank">Prometheus</a></li>
+	<li><a href="http://localhost:16686" target="_blank">Jaeger</a></li>
 	<li><a href="http://localhost:8200" target="_blank">Vault</a></li>
 	<li><a href="http://localhost:15672" target="_blank">RabbitMQ</a></li>
 </ul>
