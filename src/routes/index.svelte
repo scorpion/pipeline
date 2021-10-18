54281a5469be01be6ad741ac74e8a6680a07677a
Brett Wilcox <brett@scorpion.io>
2021-10-17 19:13:43 -0500

    cAdvisor link

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,6 +6,7 @@
 	<li><a href="http://localhost:3001" target="_blank">Grafana</a></li>
 	<li><a href="http://localhost:9090" target="_blank">Prometheus</a></li>
 	<li><a href="http://localhost:16686" target="_blank">Jaeger</a></li>
+	<li><a href="http://localhost:8080" target="_blank">cAdvisor</a></li>
 	<li><a href="http://localhost:8200" target="_blank">Vault</a></li>
 	<li><a href="http://localhost:15672" target="_blank">RabbitMQ</a></li>
 </ul>
